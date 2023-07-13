cbc9a8d8181ed6e56e50a8ae0370b14fe618b07b
Bianca Deblinger <biandeb96@gmail.com>
2023-07-13 13:45:41 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("Hola mundo")
+
+let usuario = "Bianca";
+
+console.log(usuario)
+
+let respuestaUsuario;
+respuestaUsuario = prompt("Ingrese su nombre completo por favor")
+
+let comunicado = "Aguante";
+
+console.log(comunicado)
